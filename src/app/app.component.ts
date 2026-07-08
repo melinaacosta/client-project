2b848a009992c26479fbd31930bf3afcf0a06f48
melinaacosta <melii.a@hotmail.com>
2026-07-08 15:50:03 -0300

    create components & client list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ClientListComponent } from './features/clients/client-list/client-list.component';
-import { ClientFormComponent } from './features/clients/client-form/client-form.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { CardComponent } from './shared/components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, ClientListComponent, ClientFormComponent],
+  imports: [CommonModule, ClientListComponent, NavbarComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
--- src/app/features/clients/client-list/client-list.component.ts
+++ src/app/features/clients/client-list/client-list.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-client-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatSortModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+  ],
   templateUrl: './client-list.component.html',
-  styleUrls: ['./client-list.component.scss']
+  styleUrls: ['./client-list.component.scss'],
 })
 export class ClientListComponent {
-
+  dataSource = new MatTableDataSource<any>();
+  displayedColumns = ['name', 'lastName', 'age', 'birthDate'];
 }
