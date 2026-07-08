6f49739a3b434b9a4fd317a502001a73c3150c88
melinaacosta <melii.a@hotmail.com>
2026-07-08 12:27:37 -0300

    Initial Structure & Standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ClientListComponent } from './features/clients/client-list/client-list.component';
+import { ClientFormComponent } from './features/clients/client-form/client-form.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, ClientListComponent, ClientFormComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'client-project';
