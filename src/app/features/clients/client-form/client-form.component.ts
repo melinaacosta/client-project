3728e94ed15ca456e550ca827740215d7e2bc154
melinaacosta <melii.a@hotmail.com>
2026-07-08 13:02:00 -0300

    create Client form component

--- src/app/features/clients/client-form/client-form.component.ts
+++ src/app/features/clients/client-form/client-form.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-client-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './client-form.component.html',
-  styleUrls: ['./client-form.component.scss']
+  styleUrls: ['./client-form.component.scss'],
 })
 export class ClientFormComponent {
+  clientForm: FormGroup;
 
+  constructor() {
+    this.clientForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      lastName: new FormControl(''),
+      age: new FormControl(''),
+      birthDate: new FormControl(''),
+    });
+  }
+
+  submit(): void {}
 }
